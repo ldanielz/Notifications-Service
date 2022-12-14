7c8785d490c07873b05671af11e4db5f50298c65
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-14 12:16:39 -0300

    Commit 02: Adicionando validação no POST

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -11,13 +12,14 @@ export class AppController {
     return this.prismaService.notification.findMany();
   }
   @Post()
-  async create() {
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
     await this.prismaService.notification.create({
       data: {
         id: randomUUID(),
-        content: 'Nova Solicitação de amizade',
-        category: 'social',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
     });
   }
