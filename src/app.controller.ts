af2e6eac35f483570b9d7d58d14048b00747cf46
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-14 11:57:03 -0300

    First Commit

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prismaService.notification.findMany();
+  }
+  @Post()
+  async create() {
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Nova Solicitação de amizade',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
+  }
+}
